d1b7c880cf2d0ae22c28322d0b8d70fbafb2363b
Roman Samarin <cpahdejib@gmail.com>
2018-04-12 13:57:53 +0700

    Contract updates: push, emplace, allocator fixes

--- XORList.h
+++ XORList.h
@@ -2,16 +2,16 @@
 #include <memory> 
 #include <cstdint>
 #include <algorithm> 
+#include "Utils.h"
 
 
 template <typename T, class TAllocator = std::allocator<T>>
 class LinkedList
 {
 public:
-	using RebindAlloc = typename std::allocator_traits<TAllocator>::template rebind_alloc<T>; //Почему не биндится напрямую?
+ //Почему не биндится напрямую?
 	class Node;
 
-	RebindAlloc allocator;
 	LinkedList() : LinkedList(TAllocator()) {
 	}
 
@@ -19,10 +19,15 @@ public:
 	}
 
 private:
+	//friend class Utils;
 	struct Node {
 		uintptr_t xor_pointer;
 		T value;
-		explicit Node(Node *xorPtr = nullptr) : xor_pointer(reinterpret_cast<uintptr_t>(xorPtr)) {
+		explicit Node(Node *xor_pointer = nullptr) : xor_pointer(reinterpret_cast<uintptr_t>(xor_pointer)) {
+		}
+
+		template<typename... Args>
+		Node(Args&&... args) : value(::std::forward<Args>(args)...){
 		}
 		Node(const Node&) noexcept = default;
 		Node(Node &&) noexcept = default;
@@ -31,16 +36,21 @@ private:
 		Node& operator=(Node &&) noexcept = default;
 	};
 
+
+	using RebindAlloc = typename std::allocator_traits<TAllocator>::template rebind_alloc<Node>;
+
+	RebindAlloc allocator;
+
 	template<typename Iter, typename V>
 	class Iterator {
 
 	public:
 		V& operator*() const {
-			return static_cast<NodeWithValue*>(current)->value;
+			return static_cast<Node*>(current)->value;
 		}
 
 		Iter& operator++() {
-			Node *next = reinterpret_cast<Node*>(xorPointers(previous, current->xorPtr));
+			Node *next = reinterpret_cast<Node*>(xorPointers(previous, current->xor_pointer));
 			previous = current;
 			current = next;
 			return static_cast<Iter&>(*this);
@@ -65,7 +75,7 @@ private:
 		}
 
 		Iter& operator--() {
-			Node *newPrev = reinterpret_cast<Node*>(xorPointers(previous->xorPtr, current));
+			Node *newPrev = reinterpret_cast<Node*>(_xor(previous->xor_pointer, current));
 			current = previous;
 			previous = newPrev;
 			return static_cast<Iter&>(*this);
@@ -80,7 +90,7 @@ private:
 	protected:
 		Node *previous;
 		Node *current;
-		Iterator(Node *prev = nullptr, Node *current = nullptr) noexcept : previous(prev), current(current) {
+		Iterator(Node *previous = nullptr, Node *current = nullptr) noexcept : previous(previous), current(current) {
 		}
 		Iterator(const Iterator&) noexcept = default;
 		Iterator(Iterator&&) noexcept = default;
@@ -128,13 +138,111 @@ private:
 	private:
 		friend class LinkedList<T, TAllocator>;
 		friend class iterator;
-		const_iterator(Node *prev, Node *current) noexcept : IteratorBase<const_iterator, const T>(prev, current) {
+		const_iterator(Node *previous, Node *current) noexcept : Iterator<const_iterator, const T>(previous, current) {
 		}
 	};
 	mutable Node nHead;
 	mutable Node nTail;
+	std::size_t _size = 0;
+
+	template<typename... Args>
+	Node* createNode(Args&&... args) {
+		Node *const result = std::allocator_traits<RebindAlloc>::allocate(allocator, 1);
+
+		try {
+			allocator.construct(result, std::forward<Args>(args)...);
+		}
+		catch (...) {
+			allocator.deallocate(result, 1);
+			throw;
+		}
+		return result;
+	}
+
+	static uintptr_t _xor(const uintptr_t first, const uintptr_t second) noexcept {
+		return first ^ second;
+	}
+
+	static uintptr_t _xor(Node *const first, Node *const second) noexcept {
+		return _xor(reinterpret_cast<const uintptr_t>(first), reinterpret_cast<const uintptr_t>(second));
+	}
+
+	static uintptr_t _xor(const uintptr_t first, Node *const second) noexcept {
+		return _xor(second, first);
+	}
+
+	static uintptr_t _xor(Node *const first, const uintptr_t second) noexcept {
+		return _xor(reinterpret_cast<const uintptr_t>(first), second);
+	}
+
+
+	static std::pair<iterator, iterator> _insertPrev(const_iterator position, Node *const node) noexcept{
+		node->xor_pointer = _xor(position.previous, position.current);
+		if (position.previous != nullptr) {
+			position.previous->xor_pointer = _xor(_xor(position.previous->xor_pointer, position.current), node);
+		}
+		if (position.current != nullptr) {
+			position.current->xor_pointer = _xor(_xor(position.current->xor_pointer, position.previous), node);
+		}
+		return { { position.previous, node },{ node, position.current } };
+	}
+
+
+	template <typename... Args>
+	void _push_back(Args&&... args) {
+		_insertPrev(cend(), createNode(std::forward<Args>(args)...));
+	}
+
+	template <typename... Args>
+	void _push_front(Args&&... args) {
+		_insertPrev(cbegin(), createNode(std::forward<Args>(args)...));
+	}
 
 	public:
+		//Public contract;
+
+	template <typename... Args>
+	void emplace_back(Args&&... args) {
+		_push_back(args);
+	}
+
+	template <typename... Args>
+	void emplace_front(Args&&... args) {
+		_push_front(args);
+	}
+
+	T& front() noexcept {
+		return *begin();
+	}
+
+	T& back() noexcept {
+		return *(--end());
+	}
+
+	const T& back() const noexcept {
+		return *(--cend());
+	}
+
+	const T& front() const noexcept {
+		return *cbegin();
+	}
+
+	void push_back(const T& data) {
+		_push_back(data);
+	}
+
+	void push_front(const T& data) {
+		_push_front(data);
+	}
+
+	void push_back(T &&data) {
+		_push_back(std::move(data));
+	}
+
+	void push_front(T &&data) {
+		_push_front(std::move(data));
+	}
+
 	iterator begin() noexcept {
 		return { &nHead, reinterpret_cast<Node*>(nHead.xor_pointer) };
 	}
@@ -152,10 +260,10 @@ private:
 	}
 
 	const_iterator cbegin() const noexcept {
-		return { &nHead, reinterpret_cast<Node*>(nHead.xorPtr) };
+		return { &nHead, reinterpret_cast<Node*>(nHead.xor_pointer) };
 	}
 
 	const_iterator cend() const noexcept {
-		return { reinterpret_cast<Node*>(nTail.xorPtr), &nTail };
+		return { reinterpret_cast<Node*>(nTail.xor_pointer), &nTail };
 	}
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "XORList.h"
+#include "Utils.h"
+#include <string>
 
 int main() {
-	LinkedList<int> *test_list = new LinkedList<int>();
+	LinkedList<double> *test_list = new LinkedList<double>();
 	auto test  = test_list->begin();
+	const double test1 = 10.0;
+	//test1 = "xyi";
+	test_list->push_back(test1);
+	test_list->push_back(20.0);
+	auto test3 = test_list->back();
+	auto test4 = test_list->front();
 	return 0;
 }
